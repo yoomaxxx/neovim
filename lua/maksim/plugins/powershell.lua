b988665a224915c1ebb1f6b448411666b4053a60
Maxim Yurevich <maxim.yurewitch@gmail.com>
2025-08-01 19:39:49 +0300

    feat(powershell): expand configuration for `powershell`

--- lua/maksim/plugins/powershell.lua
+++ lua/maksim/plugins/powershell.lua
@@ -3,7 +3,10 @@ return {
     "TheLeoP/powershell.nvim",
     opts = {
       bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
-      -- shell = "powershell.exe",
+      capabilities = vim.lsp.protocol.make_client_capabilities(),
+      init_options = vim.empty_dict(),
+      settings = vim.empty_dict(),
+      shell = "pwsh",
     },
   },
 }
