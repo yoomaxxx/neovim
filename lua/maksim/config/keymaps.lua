e8c5711d4d840f34698525d5de3443ee2c8cc1b5
Maksim Yurevich <exile-path-mud@duck.com>
2025-08-03 05:46:35 +0300

    feat(keymaps): add keymaps for `sniprun`

--- lua/maksim/config/keymaps.lua
+++ lua/maksim/config/keymaps.lua
@@ -151,3 +151,8 @@ vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = t
 vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
 vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
 vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+
+-- sniprun
+
+vim.api.nvim_set_keymap("v", "<space>r", "<Plug>SnipRun", { silent = true })
+vim.api.nvim_set_keymap("n", "<space>r", "<Plug>SnipRun", { silent = true })
