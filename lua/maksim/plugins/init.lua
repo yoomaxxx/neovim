3cd3bccd95b8e4a202bbfd992d10e3d36eb77b72
Maksim Yurevich <exile-path-mud@duck.com>
2025-12-11 22:02:52 +0300

    refactor(plugins): remove unused plugins
    Remove tokyonight, vim-obsession, vim-fugitive, nvim-ts-autotag, and undotree plugins

--- lua/maksim/plugins/init.lua
+++ lua/maksim/plugins/init.lua
@@ -1,42 +1,13 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd([[colorscheme tokyonight-storm]])
-    end,
-  },
-  {
-    "tpope/vim-obsession",
-  },
-  {
-    "tpope/vim-fugitive",
-  },
-  {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
     config = true,
   },
   {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
-  {
     "kevinhwang91/nvim-ufo",
     dependencies = { "kevinhwang91/promise-async" },
-  },
-  {
-    "jiaoshijie/undotree",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = true,
-    keys = {
-      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-    },
   },
   {
     "gbprod/cutlass.nvim",
