02e6c15af1f82839a4f74501c5bc9087d21f0e97
maksim <maksim@example.com>
2026-01-12 14:16:14 +0300

    refactor(telescope): move keymaps to plugin configuration

--- lua/maksim/config/keymaps.lua
+++ lua/maksim/config/keymaps.lua
@@ -127,15 +127,6 @@ vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = t
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- Telescope
-
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-
 -- Undotree
 
 vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
--- lua/maksim/plugins/telescope.lua
+++ lua/maksim/plugins/telescope.lua
@@ -2,6 +2,12 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     cond = not vim.g.vscode,
+    keys = {
+      { mode = "n", "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+      { mode = "n", "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
+      { mode = "n", "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
+      { mode = "n", "<leader>fh", "<cmd>Telescope help_tags", desc = "Telescope help tags" },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
