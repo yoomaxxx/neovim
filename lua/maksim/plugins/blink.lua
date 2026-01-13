86da44be32655cc945f0a804761dba27f3889c57
maksim <maksim@example.com>
2026-01-14 02:35:46 +0300

    feat: add blink completion plugin config

--- lua/maksim/plugins/blink.lua
+++ lua/maksim/plugins/blink.lua
@@ -5,7 +5,17 @@ return {
     version = "1.*",
     cond = not vim.g.vscode,
     opts = {
-      keymap = { preset = "default" },
+      keymap = {
+        preset = "default",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+      },
       fuzzy = { implementation = "prefer_rust_with_warning" },
       sources = {
         default = { "lazydev", "lsp", "path", "snippets", "buffer" },
